e0908efd9f88d4c2181f7f9d92744e7f5b66a318
Ayush Walia <ayush.walia.js@snapdeal.com>
2016-12-20 16:24:12 +0530

    User Controller changes

--- config/dbQueries.js
+++ config/dbQueries.js
@@ -1,5 +1,3 @@
 module.exports = {
-    LOGIN_QUERY: 'SELECT id, full_name, email, mobile FROM users WHERE email = "{email}" AND password = "{password}" AND status = "ACTIVE"',
-    REGISTER_QUERY: 'INSERT INTO users (email, password, registration_date) values ("{email}", "{password}", DATE(NOW()))',
-    USER_EXISTS: "SELECT count(*) AS count from users WHERE email = '{email}' "
+    SUBSCRIBE_USER: "INSERT INTO users (user, session_token, subscription_date) values (?, ?, DATE(NOW()))"
 };
--- controllers/user.js
+++ controllers/user.js
@@ -1,98 +1,82 @@
 var util = require('./util');
 var crypto = require("../vendor/crypto");
+var md5 = require('md5');
 var connection = require("../config/db");
 var queries = require("../config/dbQueries");
 var q = require('q');
 
-module.exports = {
-    registerSession: function (req, res) {
-        var userResponse;
-        var userData = req.query.data;
-        if (!crypto.validateAuthorization(req, res, userData)) {
-            return;
-        }
-        // User is authorized here -------------
-        // name = req.getUserName();
-        // token  = generateToken(name);
-        // pushToDB(user,token).done(){
-        //     end Call with {
-        //         token : "token",
-        //     };
-        // }
-
-
-        if(req.IV_ID.userId.toString() === userData.userId.toString()) {
-            userResponse = {
-                message: "User can't review his/her own profile",
-                success: false
-            };
-            res.json(userResponse);
-            return;
-        }
-
-        if (!util.checkIfValidInteger(res, req.query.userId) ||
-                !crypto.validateAuthorization(req, res, userData ||
-                !util.checkIfValidInteger(res, req.query.rating))) {
-            return;
-        }
-        if(!userData.description || !userData.title) {
-            userResponse = {
-                message: "Insufficient data to make a review entry",
-                success: false
-            };
-            res.json(userResponse);
-            return;
-        }
-
-        userData.description = (new Buffer(JSON.stringify(userData.description)).toString('base64'));
-        userData.title = (new Buffer(JSON.stringify(userData.title)).toString('base64'));
-        userData.isAnonymous = (userData.isAnonymous === 'true');
-
-        if(userData.rating < 1) {
-            userData.rating = 1;
-        }
-        if(userData.rating > 5) {
-            userData.rating = 5;
-        }
-        var insertQuery = queries.INSERT_REVIEWS_QUERY.replace("{userId}", userData.userId)
-            .replace("{description}", userData.description)
-            .replace("{rating}", userData.rating)
-            .replace("{title}", userData.title)
-            .replace("{reviewer}", req.IV_ID.userId)
-            .replace("{isAnonymous}", userData.isAnonymous);
-
-        connection.query(insertQuery, function (err, response) {
-            if (err) {
-                throw err;
+module.exports = (function() {
+    var sessionTokenGenerator = function(req, res) {
+        var data = {
+            userId: req.query.data.userId,
+            timeStamp: new Date().getTime()
+        };
+        return md5(data);
+    };
+    return {
+        registerSession: function (req, res) {
+            var userResponse;
+            var userData = req.query.data || {};
+            if (!crypto.validateAuthorization(req, res, userData)) {
+                return;
             }
-            if(response && response.insertId) {
-                var selectReview = queries.SELECT_REVIEWS_QUERY.replace("{id}", response.insertId);
-                connection.query(selectReview, function (err, rows) {
-                    if (err) {
-                        throw err;
-                    }
-                    if(rows && rows.length) {
-                        userResponse = {
-                            success: true,
-                            data: rows,
-                            message: ""
-                        };
-                    } else {
-                        userResponse = {
-                            success: false,
-                            message: "No rows found"
-                        };
-                    }
-                    res.json(userResponse);
-                });
+            // ------------- User is authorized here -------------
+            var sessionToken = sessionTokenGenerator(req, res);
+            var userId = req.query.data.userId;
+            if(userId) {
+                connection.query(queries.SUBSCRIBE_USER,
+                    [userId, sessionToken],
+                    function(err, results) {
+                        if (err) {
+                            throw err;
+                        }
+                        if(response && response.insertId) {
 
+                        }
+                    }
+                );
             } else {
                 userResponse = {
                     success: false,
-                    message: "No rows found"
+                    message: ""
                 };
-                res.json(userResponse);
             }
-        });
-    }
-};
+            res.json(userResponse);
+
+
+            // connection.query(insertQuery, function (err, response) {
+            //     if (err) {
+            //         throw err;
+            //     }
+            //     if(response && response.insertId) {
+            //         var selectReview = queries.SELECT_REVIEWS_QUERY.replace("{id}", response.insertId);
+            //         connection.query(selectReview, function (err, rows) {
+            //             if (err) {
+            //                 throw err;
+            //             }
+            //             if(rows && rows.length) {
+            //                 userResponse = {
+            //                     success: true,
+            //                     data: rows,
+            //                     message: ""
+            //                 };
+            //             } else {
+            //                 userResponse = {
+            //                     success: false,
+            //                     message: "No rows found"
+            //                 };
+            //             }
+            //             res.json(userResponse);
+            //         });
+            //
+            //     } else {
+            //         userResponse = {
+            //             success: false,
+            //             message: "No rows found"
+            //         };
+            //         res.json(userResponse);
+            //     }
+            // });
+        }
+    };
+})();
