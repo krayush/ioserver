a6e94780a2472d5ab295dfe53bbc012d02166a4f
Ayush Walia <ayush.walia.js@snapdeal.com>
2016-12-20 16:24:12 +0530

    User Controller changes

--- controllers/user.js
+++ controllers/user.js
@@ -19,41 +19,6 @@ module.exports = {
         //         token : "token",
         //     };
         // }
-
-
-        if(req.IV_ID.userId.toString() === userData.userId.toString()) {
-            userResponse = {
-                message: "User can't review his/her own profile",
-                success: false
-            };
-            res.json(userResponse);
-            return;
-        }
-
-        if (!util.checkIfValidInteger(res, req.query.userId) ||
-                !crypto.validateAuthorization(req, res, userData ||
-                !util.checkIfValidInteger(res, req.query.rating))) {
-            return;
-        }
-        if(!userData.description || !userData.title) {
-            userResponse = {
-                message: "Insufficient data to make a review entry",
-                success: false
-            };
-            res.json(userResponse);
-            return;
-        }
-
-        userData.description = (new Buffer(JSON.stringify(userData.description)).toString('base64'));
-        userData.title = (new Buffer(JSON.stringify(userData.title)).toString('base64'));
-        userData.isAnonymous = (userData.isAnonymous === 'true');
-
-        if(userData.rating < 1) {
-            userData.rating = 1;
-        }
-        if(userData.rating > 5) {
-            userData.rating = 5;
-        }
         var insertQuery = queries.INSERT_REVIEWS_QUERY.replace("{userId}", userData.userId)
             .replace("{description}", userData.description)
             .replace("{rating}", userData.rating)
